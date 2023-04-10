a0aaf8be05dec3aad0fe2bd32b1e4c73065f6398
edk21 <edwinmcken@gmail.com>
2023-04-10 22:27:55 +0200

    totalAmount initialization

--- routes/record.js
+++ routes/record.js
@@ -52,7 +52,8 @@ recordRoutes.route("/record/:name").get(function(req, res){
 // create a new record
 recordRoutes.route("/record/add").post(function(req, res){
     let db_connect = dbo.getDB();
-    let totalAmount = req.body.balance + totalAmount;
+    let totalAmount = 0;
+    totalAmount = req.body.balance + totalAmount;
     let newObj = {
       surname: req.body.surname,
       name: req.body.name,
