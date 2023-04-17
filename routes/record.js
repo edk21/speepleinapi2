6eedba0af2cea43d7f2c2ffea050d0f62733ee37
edk21 <edwinmcken@gmail.com>
2023-04-17 09:15:44 +0200

    updating the ObjectId implementation

--- routes/record.js
+++ routes/record.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const recordRoutes = express.Router();
 const dbo = require("../db/conn");
-const ObjectId = require("mongodb").ObjectId;
+const ObjectID = require("mongodb").ObjectID;
 const data = require("../data")
 
 //get All records
@@ -49,6 +49,7 @@ recordRoutes.route("/record/stats").get(async function(req, res){
 // get a singl record by ID
 recordRoutes.route("/record/:id").get(async function(req, res){
     let db_connect = dbo.getDB();
+    let ObjectId = new ObjectID();
     let myQuery = { _id: ObjectId( req.params.id ) };
     try {
         const record = await db_connect
@@ -141,6 +142,7 @@ recordRoutes.route("/record/stats/add").post(async function(req, res){
 // update a record
 recordRoutes.route("/update/:id").put(async function(req, res){
     let db_connect = dbo.getDB();
+    let ObjectId = new ObjectID();
     let myQuery = { _id:ObjectId( req.params.id ) };
     let newValues = {
       $set: {
@@ -187,6 +189,7 @@ recordRoutes.route("/update/:id").put(async function(req, res){
 // delete a record
 recordRoutes.route("/:id").delete(async function(req, res) {
     let db_connect = dbo.getDB();
+    let ObjectId = new ObjectID();
     let myQuery = { _id: ObjectId( req.params.id ) };
     try {
       const record = await db_connect
@@ -202,6 +205,7 @@ recordRoutes.route("/:id").delete(async function(req, res) {
 // delete a daily registration
 recordRoutes.route('/record/deleteAStat/:id').delete(async function(req, res){
     let db_connect = dbo.getDB();
+    let ObjectId = new ObjectID();
     let myQuery = { _id: ObjectId( req.params.id )};
     try {
       const record = await db_connect.collection("statDatas").deleteOne(myQuery)
